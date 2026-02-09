ec9f955a91d518c59d7ad0b604ad884ec99dc716
Chibuezem Marvinrose <rozzeymarvin32@gmail.com>
2026-02-09 02:25:49 +0100

    Add author to Article model and display articles on webpage

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -5,6 +6,7 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
