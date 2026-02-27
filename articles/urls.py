4a0c042a9ded9d2821e92b841bbec047afc75ded
Chibuezem Marvinrose <rozzeymarvin32@gmail.com>
2026-02-27 23:31:57 +0100

    Built the Articles home page, Navbar, Urls

--- articles/urls.py
+++ articles/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path("", views.home, name="home"),                 # /
+    path("articles/", views.index, name="articles"),   # /articles/
     path("articles/<slug:slug>/", views.article_detail, name="article_detail"),
 ]
--- articles/views.py
+++ articles/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from .models import Article
 
 def home(request):
-    articles = Article.objects.all()
+    articles = Article.objects.all().order_by("-created_at")[:5]
     return render(request, "articles/home.html", {"articles": articles})
 
+def index(request):
+    articles = Article.objects.all().order_by("-created_at")
+    return render(request, "articles/index.html", {"articles": articles})
 
 def article_detail(request, slug):
     article = get_object_or_404(Article, slug=slug)
