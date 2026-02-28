72a2543c52feb332264e728dbe30929b22db9134
Chibuezem Marvinrose <rozzeymarvin32@gmail.com>
2026-03-01 00:01:07 +0100

    Responsive layout and modifications

--- articles/urls.py
+++ articles/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),                 # /
-    path("articles/", views.index, name="articles"),   # /articles/
+    path("articles/", views.index, name="articles"),   # homepage
     path("articles/<slug:slug>/", views.article_detail, name="article_detail"),
 ]
