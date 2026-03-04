a56f98294dccb025c4711cf658e616c6a59580ad
Chibuezem Marvinrose <rozzeymarvin32@gmail.com>
2026-03-04 19:17:00 +0100

    Fix ALLOWED_HOSTS for Render

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y7p$wweofu)2@&am=di_l0=o&s=lcv)o_f+bp57^35e!ww6d#-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', "https://community-knowledge-hub.onrender.com/"]
 
 
 # Application definition
